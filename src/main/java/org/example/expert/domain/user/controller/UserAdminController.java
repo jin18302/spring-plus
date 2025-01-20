ebae6497bb3bbe6fe77a2092a75e3479319a4163
lee sujin <jin18302@naver.com>
2025-01-21 00:38:12 +0900

    Fix: 1-3

--- src/main/java/org/example/expert/domain/user/controller/UserAdminController.java
+++ src/main/java/org/example/expert/domain/user/controller/UserAdminController.java
@@ -1,6 +1,7 @@
 package org.example.expert.domain.user.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.example.expert.domain.user.dto.request.UserRoleChangeRequest;
 import org.example.expert.domain.user.service.UserAdminService;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class UserAdminController {
@@ -17,5 +19,6 @@ public class UserAdminController {
     @PatchMapping("/admin/users/{userId}")
     public void changeUserRole(@PathVariable long userId, @RequestBody UserRoleChangeRequest userRoleChangeRequest) {
         userAdminService.changeUserRole(userId, userRoleChangeRequest);
+        log.info("컨트롤러 메서드가 호출되었습니다");
     }
 }
